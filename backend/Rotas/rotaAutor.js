3f95a7d2a8dc452d11fecfd5d6cbc01b58187539
Rafael Damacena <fael_damacena@hotmail.com>
2024-04-02 23:32:16 -0300

    Commit Inicial

---
+++ backend/Rotas/rotaAutor.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+const rotaAutor = new Router();
+
+rotaAutor
+.post('/', )
+.get('/', )
+.get('/:termo', )
+.put('/', )
+.delete('/:', )
+
+export default rotaAutor;
---
+++ backend/Rotas/rotaBiblioteca.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+const rotaBiblioteca = new Router();
+
+rotaBiblioteca
+.post('/', )
+.get('/', )
+.get('/:termo', )
+.put('/', )
+.delete('/:', )
+
+export default rotaBiblioteca;
