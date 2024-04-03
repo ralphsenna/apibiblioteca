f2a82ec43504656d68e7a1e423de70a54c24be53
Rafael Damacena <fael_damacena@hotmail.com>
2024-04-02 23:43:46 -0300

    Commit Inicial

--- backend/Persistencia/Comandos SQL.sql
+++ backend/Persistencia/Comandos SQL.sql
@@ -1,5 +1,6 @@
 CREATE TABLE Biblioteca (
     bli_cod INT PRIMARY KEY AUTO_INCREMENT,
+    bli_cnpj VARCHAR(14) NOT NULL,
     bli_nome VARCHAR(100) NOT NULL,
     bli_endereco VARCHAR(100) NOT NULL,
     bli_telefone VARCHAR(15) NOT NULL,
@@ -23,4 +24,14 @@ CREATE TABLE Livro (
     liv_numPag INT NOT NULL,
     aut_cod INT,
     FOREIGN KEY (aut_cod) REFERENCES Autor(aut_cod)
+);
+
+CREATE TABLE Livro_Biblioteca (
+    liv_cod INT PRIMARY KEY,
+    bli_cod INT PRIMARY KEY,
+    lb_quantidade INT NOT NULL,
+    lb_condicao VARCHAR(50) NOT NULL,
+    lb_dataAquisicao DATE NOT NULL,
+    FOREIGN KEY (liv_cod) REFERENCES Livro(liv_cod),
+    FOREIGN KEY (bli_cod) REFERENCES Biblioteca(bli_cod)
 );
